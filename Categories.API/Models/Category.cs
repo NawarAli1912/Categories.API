5c44e4f69a3d3fcdc8d28f58256645334919cc69
ALI Nawar <nr.ali1912@gmail.com>
2024-06-24 17:53:20 +0200

    Add project files.

---
+++ Categories.API/Models/Category.cs
@@ -0,0 +1,47 @@
+﻿using Categories.API.Models.Primitives;
+using System.Collections.ObjectModel;
+
+namespace Categories.API.Models;
+
+public class Category : Entity
+{
+
+    private readonly Collection<Category> _subCategories = [];
+
+    public string Name { get; private set; }
+
+    public string Description { get; private set; }
+
+    public Guid? ParentCategoryId { get; private set; }
+
+    public IReadOnlyCollection<Category> SubCategories => _subCategories;
+
+
+    public static Category Create(
+        Guid id,
+        string name,
+        string description,
+        Category? Parent = null,
+        Collection<Category>? subCategories = null)
+    {
+        // validation
+        return new Category(id, name, description, Parent, subCategories ?? []);
+    }
+
+
+    private Category(Guid id) : base(id)
+    {
+    }
+
+    private Category(Guid id, string name, string description, Category? parent, Collection<Category> categories) : base(id)
+    {
+        Name = name;
+        Description = description;
+        ParentCategoryId = parent?.Id;
+        _subCategories = categories;
+    }
+
+    private Category() : base(Guid.NewGuid())
+    {
+    }
+}
---
+++ Categories.API/Models/Primitives/Entity.cs
@@ -0,0 +1,34 @@
+﻿namespace Categories.API.Models.Primitives;
+
+public class Entity : IEquatable<Entity>
+{
+    public Guid Id { get; set; }
+
+    public Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    public bool Equals(Entity? other)
+    {
+        if (other is null)
+            return false;
+
+        return Id.Equals(other.Id);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Entity other)
+        {
+            return false;
+        }
+
+        return Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+}
